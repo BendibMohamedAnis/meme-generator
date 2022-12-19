7c12c5d7ee309ac2f67cfa5745325941f9a4c1dd
Anis <anisbendib@gmail.com>
2022-12-20 00:31:02 +0100

    Added States

--- src/components/main_content.js
+++ src/components/main_content.js
@@ -1,30 +1,54 @@
 import data from "./data"
-
+import React from "react"
 export default function Main_Content(){
     let url;
+    const [count,setCount]=React.useState(0)
+   
+    function add(){
+        setCount(count+1)
+        //setCount( function(oldVal){ retrun oldVal+1 }) good practise (oldval given auto at the time of function executed)
+        //setCount( prevCount => prevCount + 1)
+    }
     
     function randomIntFromInterval(min, max) { // min and max included 
         return Math.floor(Math.random() * (max - min + 1) + min)
       }
 
     function handleClick(){
-
             let i=randomIntFromInterval(0,data.length)
             url=  data[i].meme
             document.getElementById("meme").src=url;
-
     }
+
+    function greeting(name){
+        const date= new  Date();
+        const hours = date.getHours();
+        let greet="";
+        if(hours<12){
+            greet="morning"
+        }else{
+            if(hours<17 && hours>12){
+                greet="afternoon"
+            }else{
+                greet="Night";
+            }
+        }
+        return("Good "+ greet+" " + name + " !");
+    }
+
 
     return(
         <div className="main_content" >
+            <span>{greeting("Anis")}</span>
             <form method="POST" className="forms">
                     <input type="text"  placeholder="Upper text"/>
                     <input type="text"  placeholder="Bottom text"/>
             </form>
-            <button type="button" id="new_meme_btn" onClick={handleClick} >
+            <button type="button" id="new_meme_btn" onClick={add} >
                 Get new template
                 <img src={require("../assets/doge.png")} alt="icon"/>
             </button>
+            <h2>Count : {count}</h2>
             <img src="" id="meme" alt="meme"/>
         </div>
     )
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,11 +1,13 @@
 import React from "react"
 
+
 export default function Navbar(){
     return(
         <div className="navbar">
             <span><img src={require("../assets/logo.png")} alt="logo" id="logo"/></span>
             <span id="title">Meme Generator</span>
             <span id="description">React Project</span>
+            
         </div>
     )
 }
